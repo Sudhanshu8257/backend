f8da066d19bd2a90148cc5e6fadb603838484960
Sudhanshu <110216470+Sudhanshu8257@users.noreply.github.com>
2024-02-18 22:10:10 +0530

    Update user-controller.ts

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
-import { compare, hash } from "bcrypt";
+import { compare, hash } from "bcryptjs";
 import { createToken } from "../utils/token-manager.js";
 import { COOKIE_NAME } from "../utils/constants.js";
 import { GoogleGenerativeAI } from "@google/generative-ai";
