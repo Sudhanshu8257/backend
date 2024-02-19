9c5911f6e700b6637faed6daac51722ca0baf9f8
Sudhanshu <110216470+Sudhanshu8257@users.noreply.github.com>
2024-02-19 17:30:15 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import cors from "cors";
 const app = express();
 config({ path: [".env"] });
 app.use(express.json());
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173"],headers: ["Content-Type"], credentials: true }));
 
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
