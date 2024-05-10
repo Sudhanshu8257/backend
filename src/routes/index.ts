aa28a3bf9ae78eeacab1c8c4b4b35e6bd6a1c77b
Sudhanshu <110216470+Sudhanshu8257@users.noreply.github.com>
2024-05-10 16:29:25 +0530

    Update index.ts
    added welcome message

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import userRoutes from "./user-routes.js";
 import chatRoutes from "./chat-routes.js";
-
+import { welcomeMessage } from "../utils/constants.js"
 const appRouter = Router();
 
 appRouter.use("/user", userRoutes); //domain/api/v1/user
 appRouter.use("/chat", chatRoutes); //domain/api/v1/chats
+app.get('/', (req, res) => {
+  const message = welcomeMessage();
+  res.send({ message });
+});
 
 export default appRouter;
