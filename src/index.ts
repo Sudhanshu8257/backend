70eb054f7e8c77a94846d938f3850f03c6c5d39e
Sudhanshu <110216470+Sudhanshu8257@users.noreply.github.com>
2024-05-10 16:58:54 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -19,17 +19,17 @@ app.use(express.json());
 
 app.use(cors(corsConfig));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.get("/", (req, res) => {
-  const message = welcomeMessage();
-  res.send(`<p style="background-image: linear-gradient(to right, #f77979, #9b59b6);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  font-size: 2em;
-  margin-top: 24px;
-  padding: 10px;
-  text-align: center;
-  border-radius: 5px;">${message}</p>`);
-});
+// app.get("/", (req, res) => {
+//   const message = welcomeMessage();
+//   res.send(`<p style="background-image: linear-gradient(to right, #f77979, #9b59b6);
+//   -webkit-background-clip: text;
+//   -webkit-text-fill-color: transparent;
+//   font-size: 2em;
+//   margin-top: 24px;
+//   padding: 10px;
+//   text-align: center;
+//   border-radius: 5px;">${message}</p>`);
+// });
 app.use("/api/v1", appRouter);
 const PORT = process.env.PORT || 3000;
 connectToDatabase()
