78379352642bf70e2bb93614eaabfd6525000200
sudhanshu <lohanasudhanshu8257@gmail.com>
2025-12-28 19:11:10 +0530

    updated the model

--- src/routes/ai-helper.ts
+++ src/routes/ai-helper.ts
@@ -20,7 +20,7 @@ export const getGeminiResponse = async ({
 }) => {
   try {
     const model = genAI.getGenerativeModel({
-      model: "gemini-1.5-flash",
+      model: "gemini-3-flash-preview",
       // @ts-expect-error SystemInstruction
       systemInstruction,
     });
